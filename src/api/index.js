9a34f8330cf821e2407d0b66cd8c4514805ac778
genie <genie88@163.com>
2016-09-19 09:09:30 +0800

    增加实用工具函数

---
+++ src/api/index.js
@@ -0,0 +1,59 @@
+import Vue from 'vue'
+// import Resource from 'vue-resource'
+// import Dialog from '../utils/dialog'
+
+// Vue.use(Resource)
+
+if(process.env.NODE_ENV === 'production'){
+    var baseUrl = 'http://ransj.com'
+}else{
+    var baseUrl = 'http://ransj.com'
+}
+
+export default {
+    get (option) {
+        ajax('get', option);
+    },
+
+    post (option) {
+        ajax('post', option);
+    }
+}
+
+function ajax (type, option) {
+    Vue.http[type](baseUrl + option.url,  option.data || {}, {
+        headers: option.headers || {
+            //"Content-Type": 'application/json'
+            //"X-Requested-With": "XMLHttpRequest"
+        },
+        emulateJSON: true
+        // params: option.data || {}
+    })
+    .then((response) =>{
+        if(response.ok){
+            if(response.data.ret_code == 0){
+                option.success(response.data)
+            }else if(response.data.ret_code == '-1003' || response.data.ret_code == '-1011'){
+                if(location.port == '80'){
+                    window.location.href = location.protocol + '//' + location.hostname + '/#!/login'
+                }else{
+                    window.location.href = location.protocol + '//' + location.hostname + ':' + location.port + '/#!/login'
+                }
+                /*Dialog.alert({
+                    content: response.data.ret_msg,
+                    callback: function(){
+                        window.location.href = location.protocol + '//' + location.hostname + '/#!/login'
+                    }
+                })*/
+            }else{
+                Dialog.showTip(response.data.ret_msg)
+            }
+        }
+    }, (response) => {
+        if(option.error){
+            option.error()
+        }else{
+            // Dialog.showTip('网络错误，请稍后再试!');
+        }
+    });
+}
